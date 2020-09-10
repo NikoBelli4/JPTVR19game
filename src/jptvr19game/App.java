052f4ddc9e319b872ac0b47c4ec759ccba6d49ca
pupil <pupil@ivkhk.ee>
2020-09-10 10:09:46 +0300

    Буквально почвти все сделано

--- src/jptvr19game/App.java
+++ src/jptvr19game/App.java
@@ -5,12 +5,27 @@
  */
 package jptvr19game;
 
+import java.util.Scanner;
+import java.util.Random;
+
 /**
  *
  * @author pupil
  */
 public class App {
     public void run() {
-        System.out.println("Hello");
+        System.out.println("дарова");
+        System.out.println("код загадал число, угадывай давай");
+        Random rand = new Random();
+        int rNumber = rand.nextInt(200);
+        Scanner scanner = new Scanner(System.in);
+        int myNumber = scanner.nextInt();
+        if (rNumber == myNumber) {
+            System.out.println("красава");
+        }else{
+            System.out.println("лох, правильно было " + rNumber);
+        }
+                
+                
     }
 }
