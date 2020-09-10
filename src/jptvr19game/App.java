419c5d370c7931b3bf2fee0b5ed977c12e2ad183
pupil <pupil@ivkhk.ee>
2020-09-10 10:59:32 +0300

    Буквально почвти все сделано

--- src/jptvr19game/App.java
+++ src/jptvr19game/App.java
@@ -14,18 +14,33 @@ import java.util.Random;
  */
 public class App {
     public void run() {
+        int a = 5;
         System.out.println("дарова");
-        System.out.println("код загадал число, угадывай давай\n 50 до 200");
-        Random rand = new Random();
-        int rNumber = rand.nextInt(15-1+1) + 1;
+        System.out.println("какой диапазон?");
+        System.out.println("Min: ");
         Scanner scanner = new Scanner(System.in);
-        int myNumber = scanner.nextInt();
-        if (rNumber == myNumber) {
-            System.out.println("красава");
-        }else{
-            System.out.println("лох, правильно было " + rNumber);
-        }
-                
+        int min = scanner.nextInt();
+        System.out.println("Max:");
+        int max = scanner.nextInt();
+        do {
+            if(a == 5);{
+                System.out.println("код загадал число, угадывай давай\n от " + min + " до " + max);
+                    }
+            Random rand = new Random();
+            int rNumber = rand.nextInt(max-min+1) + min;
+            int myNumber = scanner.nextInt();
+            if (rNumber == myNumber) {
+                System.out.println("красава");
+                break;
+            }else{
+                if(a <= 1){
+                    System.out.println("лох, правильно было " + rNumber);
+                    break;
+                }else if(a>1){
+                    System.out.println("попробуй еще раз");
+                }
+            }   a--;
+        }while(a >0);     
                 
     }
 }
